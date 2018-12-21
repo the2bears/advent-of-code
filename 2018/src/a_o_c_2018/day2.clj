a11ca7c49ff2d1e9ff75391e7929bde788afe383
the2bears <the2bears@gmail.com>
2018-12-21 15:56:29 -0800

    Day 2 part 2

--- 2018/src/a_o_c_2018/day2.clj
+++ 2018/src/a_o_c_2018/day2.clj
@@ -19,4 +19,43 @@
     (* (count twos) (count threes))))
 
 
+(defn same-chars
+  ([[s1 s2]]
+   (same-chars s1 s2)) 
+  ([s1 s2]
+   (loop [seq1 (seq s1)
+          seq2 (seq s2)
+          acc  []]
+     (if (seq seq1)
+       (recur (rest seq1) (rest seq2) (if-not (= (first seq1) (first seq2))
+                                        acc
+                                        (conj acc (first seq1))))
+       (apply str acc)))))
+              
+(defn diff-chars
+  [s1 s2]
+  (loop [seq1 (seq s1)
+         seq2 (seq s2)
+         acc  []]
+    (if (seq seq1)
+      (recur (rest seq1) (rest seq2) (if (= (first seq1) (first seq2))
+                                       acc
+                                       (conj acc (first seq1))))
+      acc)))
+
+
 (day2-p1 day2-data)
+;;5681
+
+(defn diff-by-one [data]
+  (for [w1 data
+        w2 data
+        :when (and (not= w1 w2)
+                   (= 1 (count (diff-chars w1 w2))))]
+    [w1 w2]))
+       
+(defn day2-p2 [data]
+  (same-chars (first (diff-by-one data))))
+
+(day2-p2 day2-data)
+;;uqyoeizfvmbistpkgnocjtwdl
