baeeb734ac02657dd68950ad429ae0252a67375a
the2bears <the2bears@gmail.com>
2018-12-23 10:09:23 -0800

    Day 5 part 2, really should revisit to optimize this or find a better way

--- 2018/src/a_o_c_2018/day5.clj
+++ 2018/src/a_o_c_2018/day5.clj
@@ -34,3 +34,26 @@
 
 (day5-p1 day5-data)
 ;;10766
+
+(def alphabet "abcdefghijhlmnopqrstuvwxyz")
+
+(defn remove-polymer [p data]
+  (let [upper-p (first (clojure.string/upper-case p))
+        lower-p (first (clojure.string/lower-case p))
+        letters (into #{} (vector upper-p lower-p))]
+    (apply str (remove #(letters %) data))))
+   
+(defn day5-p2
+  "Really needs to be optimized! Could run reactions on whole data first (once),
+   then prune letters, then re-run reactions."
+  [data alphas]
+  (let [new-reaction (fn[d p] (->> d
+                                   (remove-polymer p)
+                                   (day5-p1)))]
+    (->> (map #(vector % (new-reaction data %)) alphas)
+         (sort-by second)
+         (first)
+         (second))))
+
+;;(day5-p2 day5-data alphabet)
+;;6538
