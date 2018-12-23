e210d43dcbb3f76b9388a108675e071efc5ebc43
the2bears <the2bears@gmail.com>
2018-12-23 09:12:27 -0800

    Day 5 part 1

--- 2018/src/a_o_c_2018/day5.clj
+++ 2018/src/a_o_c_2018/day5.clj
@@ -1,10 +1,36 @@
 (ns a-o-c-2018.day5
   (:require [clojure.java.io :as io]))
 
-(def day5-data (slurp "resources/day5.txt"))
+;;drop the \newline 
+(def day5-data (drop-last (slurp "resources/day5.txt")))
 
-(defn reactive-pair? [a b]
+(def test-data "dabAcCaCBAcCcaDA")
+
+(defn reactive-pair?
+  [[a b]]
   (and (not= a b)
        (= (clojure.string/lower-case (str a))
           (clojure.string/lower-case (str b)))))
 
+(count (remove reactive-pair? (partition 2 day5-data)))
+
+(defn day5-p1 [data]
+  (let [c0 (count data)
+        pass1 (->> data
+                   (partition-all 2)
+                   (remove reactive-pair?)
+                   (flatten)
+                   (apply str))
+        c1 (count pass1)
+        pass2 (->> (rest pass1)
+                   (partition-all 2)
+                   (remove reactive-pair?)
+                   (flatten)
+                   (apply str (first pass1)))
+        c2 (count pass2)]
+    (if (= c0 c1 c2)
+      c2
+      (recur pass2))))
+
+(day5-p1 day5-data)
+;;10766
