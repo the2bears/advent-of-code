9462e077f61255b9148ef704596d196723891a7f
the2bears <the2bears@gmail.com>
2018-12-25 15:31:05 -0800

    Day 8 part 1

---
+++ 2018/src/a_o_c_2018/day8.clj
@@ -0,0 +1,39 @@
+(ns a-o-c-2018.day8
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]))
+
+(def day8-data
+  (-> "./resources/day8.txt"
+      (slurp)
+      (str/split #" ")
+      (->>
+        (map read-string))))
+
+(def test-data '(2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2))
+
+(def tree-data (atom test-data))
+
+(def drop-2 (comp rest rest))
+
+(defn build-tree []
+  (let [ch (first @tree-data)
+        md (second @tree-data)
+        _ (swap! tree-data drop-2)]
+    {:children (reduce (fn [acc _]
+                         (conj acc (build-tree)))
+                       []
+                       (range ch))
+     :meta-data (let [i (take md @tree-data)
+                      _ (dotimes [n md]
+                          (swap! tree-data rest))]
+                  i)}))
+
+(defn add-meta-data [node]
+  (let [s (apply + (:meta-data node))]
+    (+ s (reduce #(+ %1 (add-meta-data %2)) 0 (:children node)))))             
+
+(defn day8-p1 [data]
+  (let [_ (reset! tree-data data)]
+    (add-meta-data (build-tree))))
+;;36027
+
