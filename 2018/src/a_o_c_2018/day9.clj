c1ac2e71f0958426b847b7131d8928c4089c83b8
the2bears <the2bears@gmail.com>
2018-12-25 19:22:44 -0800

    Day 9 Part 1 - so slow... will play around with optimizing

---
+++ 2018/src/a_o_c_2018/day9.clj
@@ -0,0 +1,61 @@
+(ns a-o-c-2018.day9)
+
+(split-at 1 [0])
+
+(split-at 1 [0 1])
+
+(defn next-vector [^long current-pos ^long next-value v]
+  (let [special-case? (= 0 (mod next-value 23))
+        next-index (if special-case?
+                     (- current-pos 7)
+                     (+ 1 (mod (+ current-pos 1) (count v))))
+        next-index (if (< next-index 0)
+                     (+ next-index (count v))
+                     next-index)
+        [left right] (split-at (+ next-index (if special-case? 1 0))  v)
+        left (into [] left)
+        right (into [] right)
+        score (if special-case?
+                (+ next-value (last left))
+                0)]
+    [next-index
+     score
+     (->> (if special-case?
+            (into (into [] (drop-last left)) right)
+            (into (into left (vector next-value)) right))
+          (into []))]))
+
+(next-vector 3 4 [0 2 1 3])
+
+(next-vector 13 23 [0 16  8 17  4 18  9 19  2 20 10 21  5 22 11  1 12  6 13  3 14  7 15])
+
+(next-vector 6 24 [0 16  8 17  4 18 19 2 20 10 21  5 22 11  1 12  6 13  3 14  7 15]) 
+
+(defn day9-p1 [num-players num-marbles]
+  (let [players (cycle (range 1 (inc num-players)))]
+    (loop [players players
+           v [0]
+           scores {}
+           marbles (range 1 (inc num-marbles))
+           current-pos 1]
+      (if (seq marbles)
+        (let [[next-index score next-v] (next-vector current-pos
+                                                     (first marbles)
+                                                     v)
+              next-scores (update scores (first players) (fnil + 0) score)]
+          (recur (rest players)
+                 next-v
+                 next-scores
+                 (rest marbles)
+                 next-index))
+        (->> scores
+             (into [])
+             (sort-by second)
+             ((comp last last)))))))      
+                          
+;;(day9-p1 410 72059)
+;;429287
+
+(comment
+  (set! *unchecked-math* :warn-on-boxed)
+  (set! *warn-on-reflection* true))
