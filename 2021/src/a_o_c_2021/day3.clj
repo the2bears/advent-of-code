7e3bc055adb6d91e25541950e199e28a953a3a1f
William Swaney <the2bears@gmail.com>
2022-10-03 21:00:57 -0700

    2021 Day 3, part 2

--- 2021/src/a_o_c_2021/day3.clj
+++ 2021/src/a_o_c_2021/day3.clj
@@ -38,3 +38,39 @@
 (println (str "Answer for Part 1 is " part1))
 ;;3901196
 
+(defn string-to-digit-seq [c]
+  (->> c
+       (map seq)
+       (map map-to-digits)))
+
+(comment
+  (defn >= [a b]
+    (or (= a b) (> a b))))
+
+;;k-fn is the 'keep function'. > means you keep when 1 is > c
+;; < means you keep when 1 is < c
+(defn filter-nth-digit [k-fn i data]
+  (let [c (count data)
+        one-count (reduce #(+ %1 (nth %2 i)) 0 data)
+        filter-digit (if (k-fn one-count (/ c 2)) 1 0)
+        filtered-data (filter #(= (nth % i) filter-digit) data)]        
+    filtered-data))
+
+
+(defn find-life-support-rating [k-fn s]
+  (let [c (count (first s))
+        data (string-to-digit-seq s)]
+    (loop [i 0
+           rating-data data]
+      (if (or (= (count rating-data) 1) (>= i c))
+        (flatten rating-data)
+        (recur (+ i 1) (filter-nth-digit k-fn i rating-data))))))
+
+(def part2
+  (let [oxygen-generator-rating (find-life-support-rating >= data)
+        CO2-scrubber-rating (find-life-support-rating < data)
+        ogr-i (Integer/parseInt (apply str oxygen-generator-rating) 2)
+        csr-i (Integer/parseInt (apply str CO2-scrubber-rating) 2)]
+    (* ogr-i csr-i)))
+        
+(println (str "Answer for Part 2 is " part2))
